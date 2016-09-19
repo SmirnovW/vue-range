9b85893103fd3f5b439c12d36eb19d2282346060
Vladislav Smirnov <mr.bikus@yandex.ru>
2016-09-20 00:47:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * This is simple alternative of build systems like gulp and grunt.
+ * Maybe it look like not pretty but it simple for compile and developing :)
+*/
+const fs = require("fs"),
+	browserify = require('browserify'),
+	vueify = require('vueify');
+
+vueify.compiler.applyConfig({
+	"presets": ["es2015"],
+	"plugins": ["transform-runtime"]
+});
+
+var mainFile = './src/vuefy/main.js';
+var vueRangeFile = './src/vuefy/components/vue-range.vue';
+
+build(mainFile);
+
+fs.watch(mainFile, () => {
+	build(mainFile);
+});
+
+fs.watch(vueRangeFile, () => {
+	build(mainFile);
+});
+
+function build(file) {
+	browserify(file)
+		.transform(vueify)
+		.bundle()
+		.pipe(fs.createWriteStream("./build/bundle.js"));
+}
+
